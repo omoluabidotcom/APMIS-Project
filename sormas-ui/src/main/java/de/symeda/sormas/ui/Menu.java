omoluabidotcom/APMIS-Project

commit 63c01ae507ad4743292676b241e371aee9812f18

Popups sometimes don't show scrollbars #613 + Fixed statistics components overflowing each other

sormas-ui/src/main/java/de/symeda/sormas/ui/Menu.java
@@ -75,8 +75,6 @@ public class Menu extends CssLayout {
 
             }
         });
-
-        logoutMenu.addStyleName("user-menu");
         menuPart.addComponent(logoutMenu);
 
         // button for toggling the visibility of the menu when on a small screen

sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/DashboardStatisticsSubComponent.java
@@ -2,8 +2,10 @@ package de.symeda.sormas.ui.dashboard;
 
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.AbstractLayout;
 import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
@@ -15,7 +17,7 @@ import de.symeda.sormas.ui.utils.CssStyles;
 public class DashboardStatisticsSubComponent extends VerticalLayout {
 
 	// Layouts
-	private HorizontalLayout countLayout;
+	private AbstractLayout countLayout;
 	private AbstractOrderedLayout contentLayout;
 	private VerticalLayout leftContentColumnLayout;
 	private VerticalLayout rightContentColumnLayout;
@@ -58,10 +60,8 @@ public class DashboardStatisticsSubComponent extends VerticalLayout {
 	}
 	
 	public void addCountLayout() {
-		countLayout = new HorizontalLayout();
+		countLayout = new CssLayout();
 		countLayout.setWidthUndefined();
-		countLayout.setSpacing(true);
-		CssStyles.style(countLayout, CssStyles.VSPACE_4);
 		
 		addComponent(countLayout);
 		setExpandRatio(countLayout, 0);
@@ -69,6 +69,7 @@ public class DashboardStatisticsSubComponent extends VerticalLayout {
 	
 	public void addComponentToCountLayout(AbstractComponent countElement) {
 		countElement.setWidthUndefined();
+		countElement.addStyleName(CssStyles.HSPACE_RIGHT_3);
 		countLayout.addComponent(countElement);
 	}
 	

sormas-ui/src/main/webapp/VAADIN/themes/sormas/components/label.scss
@@ -76,7 +76,7 @@
 
 	// Font sizes
 	&.v-label-small {
-    	font-size: 0.8em;
+    	font-size: 0.75em;
 	}
 
 	&.v-label-medium {

sormas-ui/src/main/webapp/VAADIN/themes/sormas/components/menu.scss
@@ -20,6 +20,10 @@ $valo-menu-background-color: $s-default-color !default;
       border-bottom: 3px solid $s-critical-color;
     }
     
+    .valo-menu-part {
+      overflow-x: hidden; /* no x-scrollbar */
+    }
+    
     .valo-menu-logo {
       height: 32px !important;
       width: auto !important;

sormas-ui/src/main/webapp/VAADIN/themes/sormas/sormas.scss
@@ -92,6 +92,12 @@ $editor-caption-font-color: valo-font-color($editor-background-color, 0.5) !defa
 
 @import "../valo/valo.scss";
 
+// #613 see https://stackoverflow.com/questions/36281582/vaadin-modal-window-how-to-get-scrollbars-on-the-parent-view
+// must be outside theme mixin as per https://vaadin.com/wiki/-/wiki/Main/Adding+a+splash+screen
+.v-generated-body {
+  // change the overflow from hidden to auto
+  overflow: auto;
+}
 
 @function valo-font-color ($bg-color, $contrast: $v-font-color-contrast) {
   @if type-of($bg-color) == color {

commit 8e15a57bf3464722841acc0ab1c06e306fc5494f

Fixed logut css bug introduced by 63c01ae507ad4743292676b241e371aee9812f18 for #613

sormas-ui/src/main/java/de/symeda/sormas/ui/Menu.java
@@ -75,6 +75,8 @@ public class Menu extends CssLayout {
 
             }
         });
+
+        logoutMenu.addStyleName("user-menu");
         menuPart.addComponent(logoutMenu);
 
         // button for toggling the visibility of the menu when on a small screen