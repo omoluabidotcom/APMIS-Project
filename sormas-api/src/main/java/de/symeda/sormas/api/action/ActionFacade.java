SormasBatch/SORMAS-Project

Adds rest api for actions of events

sormas-api/src/main/java/de/symeda/sormas/api/action/ActionFacade.java
@@ -17,6 +17,9 @@
  *******************************************************************************/
 package de.symeda.sormas.api.action;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.ejb.Remote;
 
 @Remote
@@ -27,4 +30,10 @@ public interface ActionFacade {
 	ActionDto getByUuid(String uuid);
 
 	void deleteAction(ActionDto ActionDto);
+
+	List<ActionDto> getAllActiveActionsAfter(Date date);
+
+	List<ActionDto> getByUuids(List<String> uuids);
+
+	List<String> getAllActiveUuids();
 }

sormas-backend/src/main/java/de/symeda/sormas/backend/action/ActionFacadeEjb.java
@@ -18,7 +18,10 @@
 package de.symeda.sormas.backend.action;
 
 import java.sql.Timestamp;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
@@ -31,6 +34,7 @@ import de.symeda.sormas.api.action.ActionFacade;
 import de.symeda.sormas.api.user.UserRole;
 import de.symeda.sormas.backend.event.EventFacadeEjb;
 import de.symeda.sormas.backend.event.EventService;
+import de.symeda.sormas.backend.user.User;
 import de.symeda.sormas.backend.user.UserFacadeEjb;
 import de.symeda.sormas.backend.user.UserService;
 import de.symeda.sormas.backend.util.DtoHelper;
@@ -142,6 +146,33 @@ public class ActionFacadeEjb implements ActionFacade {
 
 		Action action = actionService.getByUuid(actionDto.getUuid());
 		actionService.delete(action);
+	}
+
+	@Override
+	public List<String> getAllActiveUuids() {
+
+		User user = userService.getCurrentUser();
+		if (user == null) {
+			return Collections.emptyList();
+		}
+
+		return actionService.getAllActiveUuids(user);
+	}
+
+	@Override
+	public List<ActionDto> getAllActiveActionsAfter(Date date) {
+
+		User user = userService.getCurrentUser();
+		if (user == null) {
+			return Collections.emptyList();
+		}
+
+		return actionService.getAllActiveActionsAfter(date, user).stream().map(c -> toDto(c)).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<ActionDto> getByUuids(List<String> uuids) {
+		return actionService.getByUuids(uuids).stream().map(c -> toDto(c)).collect(Collectors.toList());
 	}
 
 	@LocalBean

sormas-rest/src/main/java/de/symeda/sormas/rest/ActionResource.java
@@ -1,0 +1,79 @@
+/*******************************************************************************
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package de.symeda.sormas.rest;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import de.symeda.sormas.api.FacadeProvider;
+import de.symeda.sormas.api.PushResult;
+import de.symeda.sormas.api.action.ActionDto;
+
+/**
+ * @see <a href="https://jersey.java.net/documentation/latest/">Jersey
+ *      documentation</a>
+ * @see <a href=
+ *      "https://jersey.java.net/documentation/latest/jaxrs-resources.html#d0e2051">Jersey
+ *      documentation HTTP Methods</a>
+ *
+ */
+@Path("/actions")
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+@Consumes(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+@RolesAllowed({
+	"USER",
+	"REST_USER" })
+public class ActionResource extends EntityDtoResource {
+
+	@GET
+	@Path("/all/{since}")
+	public List<ActionDto> getAll(@PathParam("since") long since) {
+		return FacadeProvider.getActionFacade().getAllActiveActionsAfter(new Date(since));
+	}
+
+	@POST
+	@Path("/query")
+	public List<ActionDto> getByUuids(List<String> uuids) {
+
+		List<ActionDto> result = FacadeProvider.getActionFacade().getByUuids(uuids);
+		return result;
+	}
+
+	@POST
+	@Path("/push")
+	public List<PushResult> postActions(List<ActionDto> dtos) {
+
+		List<PushResult> result = savePushedDto(dtos, FacadeProvider.getActionFacade()::saveAction);
+		return result;
+	}
+
+	@GET
+	@Path("/uuids")
+	public List<String> getAllActiveUuids() {
+		return FacadeProvider.getActionFacade().getAllActiveUuids();
+	}
+}

omoluabidotcom/APMIS-Project

sormas-api/src/main/java/de/symeda/sormas/api/action/ActionFacade.java
@@ -17,6 +17,9 @@
  *******************************************************************************/
 package de.symeda.sormas.api.action;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.ejb.Remote;
 
 @Remote
@@ -27,4 +30,10 @@ public interface ActionFacade {
 	ActionDto getByUuid(String uuid);
 
 	void deleteAction(ActionDto ActionDto);
+
+	List<ActionDto> getAllActionsAfter(Date date);
+
+	List<ActionDto> getByUuids(List<String> uuids);
+
+	List<String> getAllUuids();
 }

sormas-backend/src/main/java/de/symeda/sormas/backend/action/ActionFacadeEjb.java
@@ -18,7 +18,10 @@
 package de.symeda.sormas.backend.action;
 
 import java.sql.Timestamp;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
@@ -31,6 +34,7 @@ import de.symeda.sormas.api.action.ActionFacade;
 import de.symeda.sormas.api.user.UserRight;
 import de.symeda.sormas.backend.event.EventFacadeEjb;
 import de.symeda.sormas.backend.event.EventService;
+import de.symeda.sormas.backend.user.User;
 import de.symeda.sormas.backend.user.UserFacadeEjb;
 import de.symeda.sormas.backend.user.UserService;
 import de.symeda.sormas.backend.util.DtoHelper;
@@ -145,6 +149,33 @@ public class ActionFacadeEjb implements ActionFacade {
 
 		Action action = actionService.getByUuid(actionDto.getUuid());
 		actionService.delete(action);
+	}
+
+	@Override
+	public List<String> getAllUuids() {
+
+		User user = userService.getCurrentUser();
+		if (user == null) {
+			return Collections.emptyList();
+		}
+
+		return actionService.getAllUuids(user);
+	}
+
+	@Override
+	public List<ActionDto> getAllActionsAfter(Date date) {
+
+		User user = userService.getCurrentUser();
+		if (user == null) {
+			return Collections.emptyList();
+		}
+
+		return actionService.getAllActionsAfter(date, user).stream().map(c -> toDto(c)).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<ActionDto> getByUuids(List<String> uuids) {
+		return actionService.getByUuids(uuids).stream().map(c -> toDto(c)).collect(Collectors.toList());
 	}
 
 	@LocalBean

sormas-rest/src/main/java/de/symeda/sormas/rest/ActionResource.java
@@ -1,0 +1,79 @@
+/*******************************************************************************
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package de.symeda.sormas.rest;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import de.symeda.sormas.api.FacadeProvider;
+import de.symeda.sormas.api.PushResult;
+import de.symeda.sormas.api.action.ActionDto;
+
+/**
+ * @see <a href="https://jersey.java.net/documentation/latest/">Jersey
+ *      documentation</a>
+ * @see <a href=
+ *      "https://jersey.java.net/documentation/latest/jaxrs-resources.html#d0e2051">Jersey
+ *      documentation HTTP Methods</a>
+ *
+ */
+@Path("/actions")
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+@Consumes(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+@RolesAllowed({
+	"USER",
+	"REST_USER" })
+public class ActionResource extends EntityDtoResource {
+
+	@GET
+	@Path("/all/{since}")
+	public List<ActionDto> getAll(@PathParam("since") long since) {
+		return FacadeProvider.getActionFacade().getAllActionsAfter(new Date(since));
+	}
+
+	@POST
+	@Path("/query")
+	public List<ActionDto> getByUuids(List<String> uuids) {
+
+		List<ActionDto> result = FacadeProvider.getActionFacade().getByUuids(uuids);
+		return result;
+	}
+
+	@POST
+	@Path("/push")
+	public List<PushResult> postActions(List<ActionDto> dtos) {
+
+		List<PushResult> result = savePushedDto(dtos, FacadeProvider.getActionFacade()::saveAction);
+		return result;
+	}
+
+	@GET
+	@Path("/uuids")
+	public List<String> getAllUuids() {
+		return FacadeProvider.getActionFacade().getAllUuids();
+	}
+}