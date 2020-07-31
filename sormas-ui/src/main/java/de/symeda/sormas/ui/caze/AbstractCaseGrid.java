SormasBatch/SORMAS-Project

Adds visit counts in case grid

sormas-ui/src/main/java/de/symeda/sormas/ui/caze/AbstractCaseGrid.java
@@ -34,7 +34,10 @@ import de.symeda.sormas.api.FacadeProvider;
 import de.symeda.sormas.api.Language;
 import de.symeda.sormas.api.caze.CaseCriteria;
 import de.symeda.sormas.api.caze.CaseIndexDto;
+import de.symeda.sormas.api.caze.CaseLogic;
 import de.symeda.sormas.api.caze.CaseOrigin;
+import de.symeda.sormas.api.contact.ContactLogic;
+import de.symeda.sormas.api.contact.FollowUpStatus;
 import de.symeda.sormas.api.i18n.Captions;
 import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.location.LocationDto;
@@ -55,6 +58,7 @@ import de.symeda.sormas.ui.utils.ViewConfiguration;
 public abstract class AbstractCaseGrid<IndexDto extends CaseIndexDto> extends FilteredGrid<IndexDto, CaseCriteria> {
 
 	public static final String DISEASE_SHORT = Captions.columnDiseaseShort;
+	public static final String NUMBER_OF_VISITS = Captions.CaseData_numberOfVisits;
 	public static final String COLUMN_COMPLETENESS = "completenessValue";
 
 	public AbstractCaseGrid(Class<IndexDto> beanType, CaseCriteria criteria) {
@@ -88,6 +92,24 @@ public abstract class AbstractCaseGrid<IndexDto extends CaseIndexDto> extends Fi
 		Column<IndexDto, String> diseaseShortColumn = addColumn(caze -> DiseaseHelper.toString(caze.getDisease(), caze.getDiseaseDetails()));
 		diseaseShortColumn.setId(DISEASE_SHORT);
 		diseaseShortColumn.setSortProperty(CaseIndexDto.DISEASE);
+
+		Column<IndexDto, String> visitsColumn = addColumn(entry -> {
+			if (FacadeProvider.getDiseaseConfigurationFacade().hasFollowUp(entry.getDisease())) {
+				int numberOfVisits = entry.getVisitCount();
+				int numberOfRequiredVisits = CaseLogic.getNumberOfRequiredVisitsSoFar(entry.getReportDate(), entry.getFollowUpUntil());
+				int numberOfMissedVisits = numberOfRequiredVisits - numberOfVisits;
+				// Set number of missed visits to 0 when more visits than expected have been done
+				if (numberOfMissedVisits < 0) {
+					numberOfMissedVisits = 0;
+				}
+				return String.format(I18nProperties.getCaption(Captions.formatNumberOfVisitsFormat), numberOfVisits, numberOfMissedVisits);
+			} else {
+				return "-";
+			}
+
+		});
+		visitsColumn.setId(NUMBER_OF_VISITS);
+		visitsColumn.setSortable(false);
 
 		addComponentColumn(indexDto -> {
 			Label label =
@@ -165,6 +187,7 @@ public abstract class AbstractCaseGrid<IndexDto extends CaseIndexDto> extends Fi
 				CaseIndexDto.CREATION_DATE,
 				CaseIndexDto.FOLLOW_UP_STATUS,
 				CaseIndexDto.FOLLOW_UP_UNTIL,
+				NUMBER_OF_VISITS,
 				COLUMN_COMPLETENESS))
 			.flatMap(s -> s);
 	}
@@ -183,6 +206,12 @@ public abstract class AbstractCaseGrid<IndexDto extends CaseIndexDto> extends Fi
 			this.getColumn(CaseIndexDto.OUTCOME).setHidden(false);
 		} else if (this.getColumn(CaseIndexDto.OUTCOME) != null) {
 			this.getColumn(CaseIndexDto.OUTCOME).setHidden(true);
+		}
+
+		if (getCriteria().getFollowUpStatus() == FollowUpStatus.NO_FOLLOW_UP) {
+			this.getColumn(NUMBER_OF_VISITS).setHidden(true);
+		} else {
+			this.getColumn(NUMBER_OF_VISITS).setHidden(false);
 		}
 
 		if (UserRole.isPortHealthUser(UserProvider.getCurrent().getUserRoles()) && getColumn(CaseIndexDto.HEALTH_FACILITY_NAME) != null) {

omoluabidotcom/APMIS-Project

sormas-ui/src/main/java/de/symeda/sormas/ui/caze/AbstractCaseGrid.java
@@ -35,6 +35,8 @@ import de.symeda.sormas.api.Language;
 import de.symeda.sormas.api.caze.CaseCriteria;
 import de.symeda.sormas.api.caze.CaseIndexDto;
 import de.symeda.sormas.api.caze.CaseOrigin;
+import de.symeda.sormas.api.contact.FollowUpStatus;
+import de.symeda.sormas.api.followup.FollowUpLogic;
 import de.symeda.sormas.api.i18n.Captions;
 import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.location.LocationDto;
@@ -56,6 +58,7 @@ import de.symeda.sormas.ui.utils.ViewConfiguration;
 public abstract class AbstractCaseGrid<IndexDto extends CaseIndexDto> extends FilteredGrid<IndexDto, CaseCriteria> {
 
 	public static final String DISEASE_SHORT = Captions.columnDiseaseShort;
+	public static final String NUMBER_OF_VISITS = Captions.CaseData_numberOfVisits;
 	public static final String COLUMN_COMPLETENESS = "completenessValue";
 
 	public AbstractCaseGrid(Class<IndexDto> beanType, CaseCriteria criteria) {
@@ -85,6 +88,24 @@ public abstract class AbstractCaseGrid<IndexDto extends CaseIndexDto> extends Fi
 		Column<IndexDto, String> diseaseShortColumn = addColumn(caze -> DiseaseHelper.toString(caze.getDisease(), caze.getDiseaseDetails()));
 		diseaseShortColumn.setId(DISEASE_SHORT);
 		diseaseShortColumn.setSortProperty(CaseIndexDto.DISEASE);
+
+		Column<IndexDto, String> visitsColumn = addColumn(entry -> {
+			if (FacadeProvider.getDiseaseConfigurationFacade().hasFollowUp(entry.getDisease())) {
+				int numberOfVisits = entry.getVisitCount();
+				int numberOfRequiredVisits = FollowUpLogic.getNumberOfRequiredVisitsSoFar(entry.getReportDate(), entry.getFollowUpUntil());
+				int numberOfMissedVisits = numberOfRequiredVisits - numberOfVisits;
+				// Set number of missed visits to 0 when more visits than expected have been done
+				if (numberOfMissedVisits < 0) {
+					numberOfMissedVisits = 0;
+				}
+				return String.format(I18nProperties.getCaption(Captions.formatNumberOfVisitsFormat), numberOfVisits, numberOfMissedVisits);
+			} else {
+				return "-";
+			}
+
+		});
+		visitsColumn.setId(NUMBER_OF_VISITS);
+		visitsColumn.setSortable(false);
 
 		addComponentColumn(indexDto -> {
 			Label label =
@@ -162,6 +183,7 @@ public abstract class AbstractCaseGrid<IndexDto extends CaseIndexDto> extends Fi
 				CaseIndexDto.CREATION_DATE,
 				CaseIndexDto.FOLLOW_UP_STATUS,
 				CaseIndexDto.FOLLOW_UP_UNTIL,
+				NUMBER_OF_VISITS,
 				COLUMN_COMPLETENESS))
 			.flatMap(s -> s);
 	}
@@ -180,6 +202,12 @@ public abstract class AbstractCaseGrid<IndexDto extends CaseIndexDto> extends Fi
 			this.getColumn(CaseIndexDto.OUTCOME).setHidden(false);
 		} else if (this.getColumn(CaseIndexDto.OUTCOME) != null) {
 			this.getColumn(CaseIndexDto.OUTCOME).setHidden(true);
+		}
+
+		if (getCriteria().getFollowUpStatus() == FollowUpStatus.NO_FOLLOW_UP) {
+			this.getColumn(NUMBER_OF_VISITS).setHidden(true);
+		} else {
+			this.getColumn(NUMBER_OF_VISITS).setHidden(false);
 		}
 
 		if (UserRole.isPortHealthUser(UserProvider.getCurrent().getUserRoles()) && getColumn(CaseIndexDto.HEALTH_FACILITY_NAME) != null) {