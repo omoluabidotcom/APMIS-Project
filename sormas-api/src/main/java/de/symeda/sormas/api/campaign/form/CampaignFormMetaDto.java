b1fb5ff60061cb9d9b233afa5639fb950783b210
Yahaya Yusuf <yyahaya222@yahoo.com>
2024-12-18 23:47:06 +0100

    fix: #759

--- sormas-api/src/main/java/de/symeda/sormas/api/campaign/form/CampaignFormMetaDto.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/campaign/form/CampaignFormMetaDto.java
@@ -1,6 +1,7 @@
 package de.symeda.sormas.api.campaign.form;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -13,6 +14,8 @@ import de.symeda.sormas.api.EntityDto;
 import de.symeda.sormas.api.Modality;
 import de.symeda.sormas.api.campaign.CampaignPhase;
 import de.symeda.sormas.api.i18n.Validations;
+import de.symeda.sormas.api.infrastructure.area.AreaReferenceDto;
+import de.symeda.sormas.api.infrastructure.region.RegionReferenceDto;
 import de.symeda.sormas.api.user.FormAccess;
 import de.symeda.sormas.api.utils.DataHelper;
 import de.symeda.sormas.api.utils.FieldConstraints;
@@ -34,6 +37,7 @@ public class CampaignFormMetaDto extends EntityDto {
 	public static final String FORM_NAME_PASHTO = "formname_ps_af"; 
 	public static final String FORM_NAME_DARI = "formname_fa_af"; 
 	public static final String ARCHIVED = "archived";
+	public static final String AREA = "area";
 
 	@Size(max = FieldConstraints.CHARACTER_LIMIT_SMALL, message = Validations.textTooLong)
 	private String formId;
@@ -67,11 +71,7 @@ public class CampaignFormMetaDto extends EntityDto {
 	private String fielType;
 	private String fieldCaption;
 	private String fieldExpression;
-	
-	
-
-
-
+	private Set<AreaReferenceDto> area;
 
 	public boolean getArchived() {
 		return archived;
@@ -215,6 +215,11 @@ public class CampaignFormMetaDto extends EntityDto {
 		this.districtentry = districtentry;
 	}
 	
-	
+	public Set<AreaReferenceDto> getArea() {
+		return area;
+	}
+	public void setArea(Set<AreaReferenceDto> area) {
+		this.area = area;
+	}
 	
 }
