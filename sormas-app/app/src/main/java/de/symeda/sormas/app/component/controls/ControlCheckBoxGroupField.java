6442492a6ef2124363c70dc2ef9ac71a189c1d3b
DevSeg <salamioluwasegun.a@gmail.com>
2025-11-21 17:32:59 +0100

    - updating changes

--- sormas-app/app/src/main/java/de/symeda/sormas/app/component/controls/ControlCheckBoxGroupField.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/component/controls/ControlCheckBoxGroupField.java
@@ -63,12 +63,9 @@ private Context storedContext;
 
 	// Constructors
 	public ControlCheckBoxGroupField(Context context) {
-
 		super(context);
 		this.storedContext = context;
 		this.selectedElements = new HashSet<>();
-
-
 	}
 
 	public ControlCheckBoxGroupField(Context context, AttributeSet attrs) {
@@ -99,7 +96,6 @@ private Context storedContext;
 			for (int i = 0; i < items.size(); i++) {
 				Item item = items.get(i);
 				// Use the enum value as key, display text as value (same pattern as setOptions)
-
 				System.out.println( item.getValue().toString() + "1455555item.getValue().toString()  " + item.getKey());
 				addItem(item.getValue().toString(), i, item.getKey());
 			}
